0812b5694b1139537093759211bc66fdd06a643f
Erick Marín <erick.marin.v@gmail.com>
2019-08-22 15:57:24 -0600

    PHP, CSV, JSON

---
+++ UnidadII/registro-facebook-sincrono-json/procesar.php
@@ -0,0 +1,86 @@
+<?php
+    //sleep(15);
+    //Si el archivo no existe lo crea
+    if ($_POST!=null){
+        $contenidoArchivo = file_get_contents('usuarios.json');//Retorna tooooooooodo el contenido del archivo
+        $usuarios = json_decode($contenidoArchivo, true);
+        $usuarios[] = $_POST;
+        //var_dump($usuarios);
+
+        
+        $archivo = fopen('usuarios.json','w'); //r=Read ,w=Write, w+=Read/Write, a+=Append
+        fwrite(
+                $archivo, 
+                json_encode($usuarios)."\n"
+        );
+        fclose($archivo);
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+    
+    
+    <table class="table">
+        <thead>
+            <tr>
+                <th>First name</th>
+                <th>Last name</th>
+                <th>Email</th>
+                <th>Password</th>
+                <th>Month</th>
+                <th>Day</th>
+                <th>Year</th>
+                <th>Genero</th>
+            </tr>
+        </thead>
+        <tbody>
+        
+            <?php
+                //var_dump($_POST);
+                $contenidoArchivo = file_get_contents('usuarios.json');//Retorna tooooooooodo el contenido del archivo
+                $usuarios = json_decode($contenidoArchivo, true);
+                for ($i=0; $i < sizeof($usuarios); $i++) { 
+                    echo sprintf("<tr>
+                            <td>%s</td>
+                            <td>%s</td>
+                            <td>%s</td>
+                            <td>%s</td>
+                            <td>%s</td>
+                            <td>%s</td>
+                            <td>%s</td>
+                            <td>%s</td>
+                        </tr>",
+                        $usuarios[$i]['first-name'],
+                        $usuarios[$i]['last-name'],
+                        $usuarios[$i]['email'],
+                        $usuarios[$i]['password'],
+                        $usuarios[$i]['month'],
+                        $usuarios[$i]['day'],
+                        $usuarios[$i]['year'],
+                        $usuarios[$i]['gender']
+                    );
+                }
+                /*foreach ($usuario as $llave => $valor) {
+                    # code...
+                }*/
+                
+            ?>
+        </tbody>
+    </table>
+    <pre>
+        <?php
+            // var_dump($usuarios); 
+        ?>
+    </pre>
+
+</body>
+</html>
--- UnidadII/registro-facebook-sincrono/procesar.php
+++ UnidadII/registro-facebook-sincrono/procesar.php
@@ -1,3 +1,23 @@
+<?php
+    //sleep(15);
+    //Si el archivo no existe lo crea
+    if ($_POST!=null){
+        $archivo = fopen('usuarios.csv','a+'); //r=Read ,w=Write, w+=Read/Write, a+=Append
+        fwrite(
+                $archivo, 
+                $_POST["first-name"].",".
+                $_POST["last-name"].",".
+                $_POST["email"].",".
+                $_POST["password"].",".
+                $_POST["month"].",".
+                $_POST["day"].",".
+                $_POST["year"].",".
+                $_POST["gender"]."\n"
+        );
+        fclose($archivo);
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,12 +25,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
-    <pre>
-    <?php
-        var_dump($_GET);
-    ?>
-    </pre>
+    
+    
+    <table class="table">
+        <thead>
+            <tr>
+                <th>First name</th>
+                <th>Last name</th>
+                <th>Email</th>
+                <th>Password</th>
+                <th>Month</th>
+                <th>Day</th>
+                <th>Year</th>
+                <th>Genero</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+                //var_dump($_POST);
+                $archivo = fopen('usuarios.csv','r');//Lectura
+                while(($linea = fgets($archivo))){ //Retorna una linea
+                    $partes = explode(',',$linea); //Divide una cadena utilizando el delimitador que se envia en el primera parametro, retorna un arreglo
+                    echo    "<tr>
+                                <td>$partes[0]</td>
+                                <td>$partes[1]</td>
+                                <td>$partes[2]</td>
+                                <td>$partes[3]</td>
+                                <td>$partes[4]</td>
+                                <td>$partes[5]</td>
+                                <td>$partes[6]</td>
+                                <td>$partes[7]</td>
+                            </tr>";
+
+                }
+                
+                fclose($archivo);
+            ?>
+        </tbody>
+    </table>
+
 </body>
 </html>
