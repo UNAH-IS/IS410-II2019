ee8547398907e17261dceb5b715b14e2188b7293
Erick Marín <erick.marin.v@gmail.com>
2019-08-23 15:58:29 -0600

    AJAX

--- UnidadII/registro-facebook-sincrono-json/procesar.php
+++ UnidadII/registro-facebook-sincrono-json/procesar.php
@@ -2,18 +2,20 @@
     //sleep(15);
     //Si el archivo no existe lo crea
     if ($_POST!=null){
-        $contenidoArchivo = file_get_contents('usuarios.json');//Retorna tooooooooodo el contenido del archivo
-        $usuarios = json_decode($contenidoArchivo, true);
-        $usuarios[] = $_POST;
-        //var_dump($usuarios);
+        if ($_POST['accion']=='registrar'){
+            $contenidoArchivo = file_get_contents('usuarios.json');//Retorna tooooooooodo el contenido del archivo
+            $usuarios = json_decode($contenidoArchivo, true);
+            $usuarios[] = $_POST;
+            //var_dump($usuarios);
 
-        
-        $archivo = fopen('usuarios.json','w'); //r=Read ,w=Write, w+=Read/Write, a+=Append
-        fwrite(
-                $archivo, 
-                json_encode($usuarios)."\n"
-        );
-        fclose($archivo);
+            
+            $archivo = fopen('usuarios.json','w'); //r=Read ,w=Write, w+=Read/Write, a+=Append
+            fwrite(
+                    $archivo, 
+                    json_encode($usuarios)
+            );
+            fclose($archivo);
+        }
     }
 ?>
 
